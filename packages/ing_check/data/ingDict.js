2c32f51ab376ba36847db80809d2033f436249f2
Sutrisno Suryajaya <sutris.sdp@gmail.com>
2018-11-23 13:02:28 +0800

    Make ingDict does not require `fs`
    Webpack have some problem with `fs`

--- packages/ing_check/data/ingDict.js
+++ packages/ing_check/data/ingDict.js
@@ -1,15 +1,5 @@
-const fs = require("fs");
-const path = require("path");
-
-const dirPath = path.normalize(`${__filename}/..`);
-
-const ingNameToInfoKeys = JSON.parse(
-  fs.readFileSync(`${dirPath}/ingNameToInfoKeys.json`, "utf8")
-);
-
-const infoKeyToInfoDetails = JSON.parse(
-  fs.readFileSync(`${dirPath}/infoKeyToInfoDetails.json`, "utf8")
-);
+const ingNameToInfoKeys = require("./ingNameToInfoKeys");
+const infoKeyToInfoDetails = require("./infoKeyToInfoDetails");
 
 module.exports = {
   ingNameToInfoKeys,
