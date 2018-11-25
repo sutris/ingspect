abff3694f698a65d0b2d061b0f1bc0c96e4a7ea5
Sutrisno Suryajaya <sutris.sdp@gmail.com>
2018-11-25 08:24:45 +0800

    Style search result page header

--- packages/app_ing_check/src/SearchResultPage.tsx
+++ packages/app_ing_check/src/SearchResultPage.tsx
@@ -5,19 +5,21 @@ import Logo from "./Logo";
 import SearchInput from "./SearchInput";
 import SearchResult from "./SearchResult";
 
+import "./SearchResultPage.css";
+
 interface ISearchResultPageProps {
   result: ICategorizeResult;
 }
 
 const SearchResultPage = (props: ISearchResultPageProps) => {
   return (
-    <>
-      <div className="header">
+    <div className="searchResultPage">
+      <div className="searchResultPage__header">
         <Logo inline small />
-        <SearchInput />
+        <SearchInput className="searchResultPage__input" />
       </div>
       <SearchResult result={props.result} />
-    </>
+    </div>
   );
 };
 
