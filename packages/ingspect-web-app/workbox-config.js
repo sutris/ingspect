49ba3635f0eceea4857e65c359de8c907e0a3709
Sutris <sutris.sdp@protonmail.com>
2019-04-13 15:31:05 +0800

    Make client-side routing on offline

--- packages/ingspect-web-app/workbox-config.js
+++ packages/ingspect-web-app/workbox-config.js
@@ -10,6 +10,9 @@ module.exports = {
   modifyURLPrefix: {
     "build/": ""
   },
+  // to make client-side routing work, fallback navigation to root HTML
+  navigateFallback: "/index.html",
+  navigateFallbackWhitelist: [/^\/search/, /^\/offline/, /^\/about/],
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/gh\/naptha\/([a-zA-Z0-9\s_\\.\-\(\):@]+\/)*[a-zA-Z0-9\s_\\.\-\(\):]+\.js$/,
