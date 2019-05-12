d9789256ced083ff19a396c4cf2572afbb5ba953
Sutris <sutris.sdp@protonmail.com>
2019-05-12 16:06:46 +0800

    Add default page test

--- packages/ingspect-web-app/src/App.spec.tsx
+++ packages/ingspect-web-app/src/App.spec.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { render } from "react-testing-library";
 
 import App from "./App";
 import store from "./store";
 
-it("should renders without crashing", () => {
-  const div = document.createElement("div");
+describe(App, () => {
+  it("should display home page by default", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    div
-  );
+    expect(
+      getByText(
+        "Categorize a list of ingredients by their source. Separate the ingredients with comma."
+      )
+    ).toBeInTheDocument();
+  });
 });
