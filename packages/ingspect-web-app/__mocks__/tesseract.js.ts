a9821cad97bc22785f2410df9459566767984ff0
Sutris <sutris.sdp@protonmail.com>
2019-05-23 07:41:32 +0800

    Add OCR progress page test

--- packages/ingspect-web-app/__mocks__/tesseract.js.ts
+++ packages/ingspect-web-app/__mocks__/tesseract.js.ts
@@ -1,14 +1,17 @@
 const PROGRESS_TIME = 1000;
 const RESULT_TIME = 2000;
 
+let progressTimer;
+let resultTimer;
+
 const job = {
   progress: jest.fn().mockImplementation(cb => {
-    setTimeout(() => {
+    progressTimer = setTimeout(() => {
       cb({ status: "OCR progress: recognizing", progress: 0.5 });
     }, PROGRESS_TIME);
   }),
   then: jest.fn().mockImplementation(cb => {
-    setTimeout(() => {
+    resultTimer = setTimeout(() => {
       cb({ text: "result" });
     }, RESULT_TIME);
   })
@@ -16,7 +19,10 @@ const job = {
 
 const recognizer = {
   recognize: jest.fn().mockReturnValue(job),
-  terminate: jest.fn()
+  terminate: jest.fn().mockImplementation(() => {
+    clearTimeout(progressTimer);
+    clearTimeout(resultTimer);
+  })
 };
 
 export default {
---
+++ packages/ingspect-web-app/src/OCRProgress.spec.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { fireEvent } from "react-testing-library";
+import Tesseract from "tesseract.js";
+
+import App from "./App";
+import { renderWithAppContext } from "./test/utils";
+
+describe("OCR Progress page", () => {
+  it("should cancel current OCR job", () => {
+    jest.useFakeTimers();
+    const { getByText, getByTestId } = renderWithAppContext(<App />);
+    const mockTerminate = Tesseract.create().terminate;
+    const inputElem = getByTestId("picture-taker-input");
+    const file = new File(["picture_blob"], "picture_taken.png", {
+      type: "image/png"
+    });
+    fireEvent.change(inputElem, { target: { files: [file] } });
+    jest.advanceTimersByTime(1000);
+
+    fireEvent.click(getByText("Cancel"));
+
+    expect(mockTerminate).toHaveBeenCalledTimes(1);
+  });
+
+  it("should navigate back to home page", () => {
+    jest.useFakeTimers();
+    const { getByText, getByTestId, history } = renderWithAppContext(<App />);
+    const inputElem = getByTestId("picture-taker-input");
+    const file = new File(["picture_blob"], "picture_taken.png", {
+      type: "image/png"
+    });
+    fireEvent.change(inputElem, { target: { files: [file] } });
+    jest.advanceTimersByTime(1000);
+
+    fireEvent.click(getByText("Cancel"));
+
+    expect(history.location.pathname).toEqual("/");
+  });
+});
