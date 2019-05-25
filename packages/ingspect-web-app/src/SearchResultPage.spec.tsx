f63debc4e4b6ca66233989eb713562310e0c9b19
Sutris <sutris.sdp@protonmail.com>
2019-05-25 13:17:38 +0800

    Add more test case to search result page

--- packages/ingspect-web-app/src/SearchResultPage.spec.tsx
+++ packages/ingspect-web-app/src/SearchResultPage.spec.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { fireEvent } from "react-testing-library";
 
 import SearchResultPage from "./SearchResultPage";
 import { renderWithAppContext } from "./test/utils";
 
 describe("Search Result page", () => {
+  it("should render search input field", () => {
+    const { getByTestId } = renderWithAppContext(<SearchResultPage />);
+
+    expect(getByTestId("search-input")).toBeInTheDocument();
+  });
+
+  it("should render picture taker button", () => {
+    const { getByTestId } = renderWithAppContext(<SearchResultPage />);
+
+    expect(getByTestId("picture-taker")).toBeInTheDocument();
+  });
+
+  it("should render a button for going back to home page", () => {
+    const { getByText, history } = renderWithAppContext(<SearchResultPage />);
+
+    const backButton = getByText("Back");
+
+    expect(backButton).toBeInTheDocument();
+
+    fireEvent.click(backButton);
+
+    expect(history.location.pathname).toEqual("/");
+  });
+
   it("should re-search with the value from query string if the URL is search result page", () => {
     const { getByText, history } = renderWithAppContext(<SearchResultPage />);
 
